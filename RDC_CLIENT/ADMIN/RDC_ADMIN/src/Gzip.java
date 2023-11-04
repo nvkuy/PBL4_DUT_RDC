26dd0bf5b45fd601d88baed9575096ab6bd43d42
nvkuy <nguyenvukhanhuy@gmail.com>
2023-11-04 11:17:39 +0700

    Fix bug encode decode

--- RDC_CLIENT/ADMIN/RDC_ADMIN/src/Gzip.java
+++ RDC_CLIENT/ADMIN/RDC_ADMIN/src/Gzip.java
@@ -2,18 +2,29 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStreamReader;
-import java.util.Base64;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class Gzip {
 
-    private static String encode(byte[] data) {
-        return Base64.getEncoder().encodeToString(data);
+//    private static String encode(byte[] data) {
+//        return Base64.getEncoder().encodeToString(data);
+//    }
+//
+//    private static byte[] decode(String data) {
+//        return Base64.getDecoder().decode(data);
+//    }
+
+    private static final Charset UTF8_CHARSET = StandardCharsets.UTF_8;
+
+    private static String encode(byte[] bytes) {
+        return new String(bytes, UTF8_CHARSET);
     }
 
-    private static byte[] decode(String data) {
-        return Base64.getDecoder().decode(data);
+    private static byte[] decode(String string) {
+        return string.getBytes(UTF8_CHARSET);
     }
 
     public static String compress(String str) throws Exception {
